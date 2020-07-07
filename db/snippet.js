391795bd5b942e15915450dff94f8f8e4cbc0b16
Andrey Yamanov <tenphi@gmail.com>
2020-07-07 12:13:42 +0300

    add start command | snippet: limit length

--- db/snippet.js
+++ db/snippet.js
@@ -1,7 +1,10 @@
 import mongoose, { Schema } from './index.js';
 
 const snippetSchema = new Schema({
-  code: String,
+  code: {
+    type: String,
+    maxlength: 65025,
+  },
 });
 
 export const Snippet = mongoose.model('Snippet', snippetSchema);
